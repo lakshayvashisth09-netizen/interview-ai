4d6f109c3be580e9a0af20ea744d60a0666f9f58
lakshayvashisth09-netizen <lakshayvashisth09@gmail.com>
2026-03-21 20:47:00 +0530

    Update auth.middleware.js

--- Backend/src/middlewares/auth.middleware.js
+++ Backend/src/middlewares/auth.middleware.js
@@ -1,40 +1,51 @@
-import jwt from "jsonwebtoken"
-import tokenBlacklistModel from "../models/blacklist.model.js"
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import Blacklist from "../models/blacklist.model.js";
 
-
-
-export async function authUser(req, res, next) {
-
-    const token = req.cookies.token
+export const authUser = async (req, res, next) => {
+  try {
+   
+    const token = req.cookies.token;
 
     if (!token) {
-        return res.status(401).json({
-            message: "Token not provided."
-        })
+      return res.status(401).json({
+        success: false,
+        message: "Unauthorized - No token",
+      });
     }
 
-    const isTokenBlacklisted = await tokenBlacklistModel.findOne({
-        token
-    })
+    
+    const isBlacklisted = await Blacklist.findOne({ token });
 
-    if (isTokenBlacklisted) {
-        return res.status(401).json({
-            message: "token is invalid"
-        })
+    if (isBlacklisted) {
+      return res.status(401).json({
+        success: false,
+        message: "Token expired / blacklisted",
+      });
     }
 
-    try {
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+   
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-        req.user = decoded
+    
+    const user = await User.findById(decoded.userId).select("-password");
 
-        next()
-
-    } catch (err) {
-
-        return res.status(401).json({
-            message: "Invalid token."
-        })
+    if (!user) {
+      return res.status(401).json({
+        success: false,
+        message: "User not found",
+      });
     }
 
-}
+    
+    req.user = user;
+
+    next();
+
+  } catch (err) {
+    return res.status(401).json({
+      success: false,
+      message: "Invalid token",
+    });
+  }
+};
